6d0efe0441920660360c726e1bc1bffcf081479a
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2023-06-13 13:55:48 +0900

    add github copilot

--- mappings.lua
+++ mappings.lua
@@ -1,2 +1,6 @@
 -- redo
 vim.api.nvim_set_keymap("n", "<S-u>", "<C-r>", { noremap = true })
+
+-- copilot
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
